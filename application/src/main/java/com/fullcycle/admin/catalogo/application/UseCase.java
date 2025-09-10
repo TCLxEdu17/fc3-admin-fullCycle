c6b3d56fafacf0566d81d13a9aa08ffc8014915c
tclxedu17 <designs.eduardog@gmail.com>
2025-09-09 22:24:55 -0300

    create UseCase

--- application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
@@ -1,10 +1,8 @@
 package com.fullcycle.admin.catalogo.application;
 
 
-import com.fullcycle.admin.catalogo.domain.Category.Category;
+public abstract class UseCase<IN, OUT> {
 
-public abstract class UseCase<IN ,OUT> {
+    public abstract OUT execute(IN anIn);
 
-    public abstract OUT execute (IN anIn);
-
-    }
+}
