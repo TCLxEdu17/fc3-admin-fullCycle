74657ef576a26e9dd9d84d01b481139bc8c65c19
tclxedu17 <designs.eduardog@gmail.com>
2025-09-10 22:38:32 -0300

    commit test

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -0,0 +1,48 @@
+package com.fullcycle.admin.catalogo.application.category.update;
+
+
+import com.fullcycle.admin.catalogo.domain.Category.Category;
+import com.fullcycle.admin.catalogo.domain.Category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.Category.CategoryID;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static net.bytebuddy.utility.JavaModule.of;
+import static org.mockito.AdditionalMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultUpdateCategoryUseCase useCase;
+
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidCommand_whenUpdateCategoryUseCase_thenReturnCategory() {
+
+        final var aCategory =  Category.newCategory("nam", null, true);
+        final var expectedId = aCategory.getId().getValue();
+        final var expectedName = "name";
+        final var expectedDescription = "description";
+        final boolean expectedIsActive = true;
+
+        final var aCommand = UpdateCategoryCommand.with(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        when(categoryGateway.findById(CategoryID.from(Mockito.eq(expectedId))
+                .)
+                (of(aCategory));
+
+    }
+}
